494418cbd0cb6557d11e3c61f481fea3f179212a
Natasha England-Elbro <natasha@natashaee.me>
2024-04-02 05:41:10 +0100

    chore: move from individual crates to single crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 use diagnostics::FatalErrorEmitted;
 
 pub mod alloc;
+pub mod asm;
 pub mod context;
 pub mod copy_arrayvec;
 pub mod diagnostics;
+pub mod frontend;
 pub mod ir;
 pub mod ops;
+pub mod pir;
 pub mod properties;
 pub mod register;
 pub mod span;
+pub mod ty;
 
 pub enum PError<E> {
     DiagnosticEmitted(FatalErrorEmitted),
